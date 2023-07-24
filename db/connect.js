8ad6e0a1faf93481821ff9c35beb0225ce31c45b
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-07-24 13:31:42 -0300

    added my tables into workbench. placed code in schema.sql

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password:'',
-    database: 'employeeTrackerdb'
+    database: 'employeeTracker_db'
 });
 
 module.exports = db;
